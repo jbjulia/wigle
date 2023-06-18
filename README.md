ca03003377f9e90f832022baa548b781c1ed7abe
Joseph Julian <joseph.b.julian@gmail.com>
2023-06-18 16:19:40 -0700

    Major fixes and improvements

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
-
----
-
 # WiGLE Network Data Retriever
 
 ## Project Description
 
-WiGLE Network Data Retriever is a Python script for querying and retrieving wireless network information within specified geographic areas from the WiGLE database. The WiGLE platform aggregates information on wireless networks (Wi-Fi, cell, bluetooth) across the globe and provides this data through their API. This script makes use of the WiGLE API to fetch data about wireless networks in specified locations such as Tacoma, Olympia, and Seattle, and presents it in both a human-readable table format and a CSV file.
+WiGLE Network Data Retriever is a Python script for querying and retrieving wireless network information within specified geographic areas from the WiGLE database. The WiGLE platform aggregates information on wireless networks (Wi-Fi, cell, Bluetooth) across the globe and provides this data through their API. This script makes use of the WiGLE API to fetch data about wireless networks in specified locations such as Tacoma, Olympia, and Seattle, and presents it in both a human-readable table format and a CSV file.
 
 ## Features
 
@@ -15,15 +12,20 @@ WiGLE Network Data Retriever is a Python script for querying and retrieving wire
 - Cleans data to remove non-printable characters.
 - Outputs the results in a well-formatted table for display in the console.
 - Saves the retrieved data to a CSV file for further analysis or processing.
+- Ensures that no empty CSV file is saved if no results are retrieved.
 
 ## Usage
 
 1. Clone the repository to your local machine.
 2. Make sure you have Python 3.x installed. Also, install the required libraries by running `pip install -r requirements.txt`.
-3. Set the environment variable `WIGLE_API_TOKEN` with your WiGLE API token.
+3. Set the environment variable `WIGLE_API_TOKEN` with your WiGLE API token, or you can pass it as a command line argument using `-api_token`.
 4. Run the script and pass the desired location as an argument. For example, for retrieving data for Tacoma, use:
    ```sh
    python wigle.py -location tacoma
+   ```
+   Optionally, you can pass your API token through the command line like this:
+   ```sh
+   python wigle.py -location tacoma -api_token YOUR_API_TOKEN
    ```
 
 ## Dependencies
@@ -41,5 +43,3 @@ WiGLE Network Data Retriever is a Python script for querying and retrieving wire
 ## Disclaimer
 
 Please note that accessing or collecting data from wireless networks might have legal and ethical implications. It is essential to ensure that you comply with the laws and regulations of your country or region and respect privacy and data protection rules.
-
----
--- wigle.py
+++ wigle.py
@@ -1,4 +1,5 @@
 import argparse
+import datetime
 import os
 import time
 
@@ -14,164 +15,194 @@ def send_request(
     longrange1,
     longrange2,
     location,
+    request_delay=5,
     max_retries=3,
-    retry_delay=60,
 ):
     """
     Send a request to the Wigle API to search for network data for the specified
-    latitude and longitude ranges and print the results as a pretty table.
+    latitude and longitude ranges, and print the results as a pretty table.
 
-    :param req_headers: The headers to be used in the API request.
-    :param latrange1: The lower latitude bound.
-    :param latrange2: The upper latitude bound.
-    :param longrange1: The lower longitude bound.
-    :param longrange2: The upper longitude bound.
-    :param location: The location of the upper/lower lat/long bound.
-    :param max_retries: The maximum number of retries in case of a 429 status code.
-    :param retry_delay: The number of seconds to wait before retrying.
+    :param req_headers: dict
+        The headers to be used in the API request.
+    :param latrange1: float
+        The lower latitude bound.
+    :param latrange2: float
+        The upper latitude bound.
+    :param longrange1: float
+        The lower longitude bound.
+    :param longrange2: float
+        The upper longitude bound.
+    :param location: str
+        The name of the location.
+    :param request_delay: int, optional
+        The number of seconds to wait between API requests.
+    :param max_retries: int, optional
+        The maximum number of retries on connection errors.
     """
-    # Set up the initial API query parameters
+    base_url = "https://api.wigle.net/api/v2/network/search"
     query_params = {
         "latrange1": latrange1,
         "latrange2": latrange2,
         "longrange1": longrange1,
         "longrange2": longrange2,
+        "first": 100,
     }
 
+    total_results = []
+    current_count = 0
+    total_count = None
+
+    print("Retrieving results...")
     retries = 0
 
     while retries < max_retries:
         try:
-            # Make the initial API request
-            response = requests.get(
-                "https://api.wigle.net/api/v2/network/search",
-                params=query_params,
-                headers=req_headers,
-            )
+            response = requests.get(base_url, params=query_params, headers=req_headers)
+            status_code = response.status_code
 
-            # Check if the response was successful
-            if response.status_code == 200:
-                results = response.json()["results"]
-                # Convert to DataFrame
-                df = pd.DataFrame(results)
+            if status_code == 200:
+                data = response.json()
+                results = data["results"]
+                total_results.extend(results)
+                current_count += len(results)
 
-                # Convert DataFrame to PrettyTable and print
-                print(pretty_table(df))
+                if total_count is None:
+                    total_count = data["totalResults"]
 
-                # Save the DataFrame to a CSV file with an escape character
-                df.to_csv(
-                    f"tests/{location}_{latrange1}_{longrange1}.csv",
-                    index=False,
-                    escapechar="\\",
-                )
+                print(f"Retrieved {current_count} out of {total_count} results")
+
+                if current_count >= total_count:
+                    break
+
+                query_params["start"] = current_count
+                time.sleep(request_delay)
+
+            elif status_code == 401:
+                print("Unauthorized. Check your API token.")
                 return
-            elif response.status_code == 429:
-                print("Too Many Requests. Waiting before retrying...")
-                time.sleep(retry_delay)
-                retries += 1
+
+            elif status_code == 429:
+                print("Too many requests. Saving the current results.")
+                break
+
             else:
-                print("Error: Received response with status code", response.status_code)
+                print(f"Error {status_code}: Failed to retrieve results.")
                 return
+
+        except requests.exceptions.ConnectionError:
+            print("Connection Error. Retrying...")
+            retries += 1
+            time.sleep(request_delay)
+
         except Exception as e:
-            print("An error occurred:", str(e))
+            print(f"An error occurred: {e}")
             return
+
+    # Save results to a CSV file if there are any results
+    if total_results:
+        df = pd.DataFrame(total_results)
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        filename = f"tests/{location}_{latrange1}_{longrange1}_{timestamp}.csv"
+        df.to_csv(
+            filename,
+            index=False,
+            mode="a",
+            header=not os.path.isfile(filename),
+            escapechar="\\",
+        )
+
+        print(f"All results retrieved. Total results: {current_count}")
+
+        # Display results as a pretty table
+        print(pretty_table(df))
+    else:
+        print("No results were retrieved.")
 
 
 def pretty_table(df):
     """
     Convert a DataFrame into a PrettyTable.
 
-    :param df: DataFrame to be converted.
-    :return: A PrettyTable object containing the data from the DataFrame.
+    :param df: pandas.DataFrame
+        DataFrame to be converted.
+    :return: PrettyTable
+        A PrettyTable object containing the data from the DataFrame.
     """
-    # Create a PrettyTable instance
     pt = PrettyTable()
-
-    # Add columns
     pt.field_names = df.columns.tolist()
 
-    # Function to clean non-printable characters
+    # Clean non-printable characters
     def clean_string(s):
-        if isinstance(s, str):
-            # This removes non-printable characters and replaces non-UTF-8 characters
-            return "".join(
+        return (
+            "".join(
                 c if c.isprintable() else "?"
                 for c in s.encode("ascii", "replace").decode()
             )
-        else:
-            return s
+            if isinstance(s, str)
+            else s
+        )
 
-    # Add rows
+    # Add rows to PrettyTable
     for _, row in df.iterrows():
-        cleaned_row = [clean_string(item) for item in row]
-        pt.add_row(cleaned_row)
+        pt.add_row([clean_string(item) for item in row])
 
-    # Return the PrettyTable
     return pt
-
-
-def set_environment_variable(name, value):
-    """
-    Set an environment variable.
-
-    :param name: The name of the environment variable.
-    :param value: The value of the environment variable.
-    """
-    # Setting the environment variable
-    os.environ[name] = value
 
 
 if __name__ == "__main__":
     # Parse command line arguments
     parser = argparse.ArgumentParser(
-        description="Request network data for a specified location"
+        description="Request network data for a specified location."
     )
     parser.add_argument(
-        "-location", choices=["tacoma", "olympia", "seattle"], help="select location"
+        "-location",
+        choices=["tacoma", "olympia", "seattle"],
+        help="Select a location: tacoma, olympia, or seattle.",
     )
+    parser.add_argument("-api_token", help="Your API token for Wigle.")
+
     args = parser.parse_args()
 
-    # Set API token as environment variable
-    set_environment_variable(
-        "WIGLE_API_TOKEN",
-        "[ YOUR_API_TOKEN_GOES_HERE ]",
-    )
+    # Get the API token from the environment variable or the command line argument
+    api_token = os.environ.get("WIGLE_API_TOKEN") or args.api_token
 
-    # Retrieve the API token from environment variable
-    api_token = os.environ.get("WIGLE_API_TOKEN")
+    # If API token is not provided through environment variable or argument, prompt user
+    if not api_token:
+        api_token = input("Please enter your API token for Wigle: ")
 
-    # Set headers
     headers = {"Authorization": f"Basic {api_token}"}
 
-    # Send request for the selected location
-    if args.location == "tacoma":
-        # Tacoma, WA
+    # Latitude and Longitude ranges for specified locations
+    location_ranges = {
+        "tacoma": {
+            "latrange1": 47.2,
+            "latrange2": 47.3,
+            "longrange1": -122.5,
+            "longrange2": -122.4,
+        },
+        "olympia": {
+            "latrange1": 47.0,
+            "latrange2": 47.1,
+            "longrange1": -122.9,
+            "longrange2": -122.8,
+        },
+        "seattle": {
+            "latrange1": 47.6,
+            "latrange2": 47.7,
+            "longrange1": -122.3,
+            "longrange2": -122.2,
+        },
+    }
+
+    # Get the selected location ranges and send request
+    if args.location in location_ranges:
+        ranges = location_ranges[args.location]
         send_request(
             headers,
-            latrange1=47.2,
-            latrange2=47.3,
-            longrange1=-122.5,
-            longrange2=-122.4,
-            location=args.location,
-        )
-    elif args.location == "olympia":
-        # Olympia, WA
-        send_request(
-            headers,
-            latrange1=47.0,
-            latrange2=47.1,
-            longrange1=-122.9,
-            longrange2=-122.8,
-            location=args.location,
-        )
-    elif args.location == "seattle":
-        # Seattle, WA
-        send_request(
-            headers,
-            latrange1=47.6,
-            latrange2=47.7,
-            longrange1=-122.3,
-            longrange2=-122.2,
+            latrange1=ranges["latrange1"],
+            latrange2=ranges["latrange2"],
+            longrange1=ranges["longrange1"],
+            longrange2=ranges["longrange2"],
             location=args.location,
         )
     else:
